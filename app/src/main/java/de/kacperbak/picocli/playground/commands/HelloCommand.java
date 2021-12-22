322dd31ce332c2af14705db0d29113f9ff89183c
Kacper Bak <github@kacperbak.de>
2021-12-22 21:33:26 +0100

    renaming

--- app/src/main/java/de/kacperbak/picocli/playground/commands/HelloCommand.java
+++ app/src/main/java/de/kacperbak/picocli/playground/commands/HelloCommand.java
@@ -6,13 +6,12 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-import java.util.Optional;
 import java.util.concurrent.Callable;
 
 @Command( name = "hello", description = "Let's say something the UNIX way", mixinStandardHelpOptions = true, version = "1.0")
 public class HelloCommand implements Callable<Integer> {
 
-    static class ExclusiveOptions {
+    static class ExclusiveSayOptions {
         @Option(names = {"-cw", "--cowSay"}, description = "Enable the Cow to say something")
         boolean cowSay;
 
@@ -20,22 +19,22 @@ public class HelloCommand implements Callable<Integer> {
         boolean tuxSay;
     }
 
-    // note: using Optional<ExclusiveOptions> type here results in a reflection exc
+    // Note: using Optional<ExclusiveSayOptions> type here results in a reflection exc
     @ArgGroup(exclusive = true, multiplicity = "0..1")
-    ExclusiveOptions eoo;
+    ExclusiveSayOptions exclusiveSayOption;
 
     @Parameters( index = "0", description = "The message to say", defaultValue = "<your message here>")
     String message;
 
     @Override
     public Integer call() {
-        if (eoo == null){
+        if (exclusiveSayOption == null){
             return HelloService.unixSay(message);
         }
-        if (eoo.cowSay){
+        if (exclusiveSayOption.cowSay){
             return HelloService.cowSay(message);
         }
-        if (eoo.tuxSay){
+        if (exclusiveSayOption.tuxSay){
             return HelloService.tuxSay(message);
         }
         return 1;
